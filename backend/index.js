5f59702533266c420a331648dbb9102e10510b52
Abhishek31Sahu <abhishek31102006@outlook.com>
2025-09-07 18:09:17 +0530

    build for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ import courses from "./router/course.js";
 import user from "./router/user.js";
 import contact from "./router/contact.js";
 import cors from "cors";
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
+
 app.use(
   cors({
     origin: "http://localhost:3000",
