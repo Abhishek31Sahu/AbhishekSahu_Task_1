8291966e3be312afb304fd29ec6dedbbb89c6da9
Abhishek31Sahu <abhishek31102006@outlook.com>
2025-09-07 19:46:11 +0530

    Update Adminlogin.jsx

--- frontend/src/component/Adminlogin.jsx
+++ frontend/src/component/Adminlogin.jsx
@@ -39,7 +39,7 @@ function Adminlogin() {
     console.log(info);
     try {
       const result = await axios.post(
-        "http://localhost:5000/api/login",
+        "https://abhisheksahu-task-1-2.onrender.com/api/login",
         info,
         {
           headers: {
