fbc18325e33e511ccaadb9ae4a8f2e39311ac2d5
Abhishek31Sahu <abhishek31102006@outlook.com>
2025-09-07 19:55:28 +0530

    Update courseInfo.jsx

--- frontend/src/component/courseInfo.jsx
+++ frontend/src/component/courseInfo.jsx
@@ -25,7 +25,7 @@ function CourseInfo() {
     }
     const getInfo = async () => {
       try {
-        const res = await axios.get(`http://localhost:5000/api/courses/${id}`);
+        const res = await axios.get(`https://abhisheksahu-task-1-2.onrender.com/api/courses/${id}`);
         const { success, message, data } = res.data;
         if (success) {
           handleSuccess(message);
